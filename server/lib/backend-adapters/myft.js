b11c152cec4b9a08bff26e4d533c15058b7a7087
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-07-11 15:34:34 +0100

    Tidy myFT adapter

--- server/lib/backend-adapters/myft.js
+++ server/lib/backend-adapters/myft.js
@@ -13,7 +13,7 @@ export default class {
 		const cacheKey = `${this.type}.all-relationship.${uuid}:relationship=${relationship}:model=${model}:limit=${limit}`;
 		const fetcher = () =>
 			myftClient.getAllRelationship('user', uuid, relationship, model, { limit })
-				.then(res => res.items)
+				.then(results => results.items.filter(nonEmpty))
 				.catch(err => {
 					// don't log if it's just a 'no user data' error
 					if (err.message !== 'No user data exists') {
@@ -28,14 +28,26 @@ export default class {
 	getViewed (uuid, { limit = 10, ttl = 60 }) {
 		const cacheKey = `${this.type}.viewed.${uuid}:limit=${limit}`;
 		const fetcher = () =>
-			myftClient.fetchJson('GET', `next/popular-concepts/${uuid}`)
-				.then(results =>
-					results.viewed
-						.filter(nonEmpty)
-						.slice(0, limit)
-				)
+			myftClient.fetchJson('GET', `next/popular-concepts/${uuid}`, { limit })
+				.then(results => results.viewed.filter(nonEmpty))
 				.catch(err => {
 					logger.error('Failed getting viewed from myFT', err, { uuid });
+					return [];
+				});
+
+		return this.cache.cached(cacheKey, ttl, fetcher);
+	}
+
+	getRecommendedTopics (uuid, { limit = 10, ttl = 60 }) {
+		const cacheKey = `${this.type}.recommended-topics.${uuid}:limit=${limit}`;
+		const fetcher = () =>
+			myftClient.fetchJson('GET', `recommendation/user/${uuid}/concept`, { limit })
+				.then(results => results.items.filter(nonEmpty))
+				.catch(err => {
+					// don't log if it's just a 'no user data' error
+					if (err.message !== 'No user data exists') {
+						logger.error('Failed getting recommended topics from myFT', err, { uuid });
+					}
 					return [];
 				});
 
@@ -63,24 +75,9 @@ export default class {
 				.then(results => results.items.filter(nonEmpty))
 				.catch(err => {
 					logger.error('Failed getting most followed topics from myFT', err);
-					logger.error(err);
 					return [];
 				});
 
 		return this.cache.cached(cacheKey, ttl, fetcher);
 	}
-
-	getRecommendedTopics (uuid, { limit = 10, ttl = 60 }) {
-		const cacheKey = `${this.type}.recommended-topics.${uuid}:limit=${limit}`;
-		const fetcher = () =>
-			myftClient.fetchJson('GET', `recommendation/user/${uuid}/concept`, { limit })
-				.then(results => results.items.filter(nonEmpty))
-				.catch(err => {
-					logger.error('Failed getting recommended topics from myFT', err, { uuid });
-					return [];
-				});
-
-		return this.cache.cached(cacheKey, ttl, fetcher);
-	}
-
 }
