c53f5b2e0edce9faca65eccd35f6f739d018cfd9
Rhys Evans <rhys.evans@ft.com>
2016-10-05 14:55:44 +0100

    response time logging for redis

--- server/lib/caches/redis-cache.js
+++ server/lib/caches/redis-cache.js
@@ -10,15 +10,17 @@ export default class {
 
 	cached (key, ttl, fetcher) {
 		const metricsKey = key.split('.')[0];
-
+		const start = Date.now();
 		return this.redis
 			.get(key)
 			.then(data => {
 				if (data !== null) {
 					// we have fresh data
+					metrics.histogram(`cache.${metricsKey}.hit.time`, Date.now() - start)
 					metrics.count(`cache.${metricsKey}.hit`, 1);
 					return JSON.parse(data);
 				}
+				metrics.histogram(`cache.${metricsKey}.miss.time`, Date.now() - start)
 				metrics.count(`cache.${metricsKey}.miss`, 1);
 
 				// we don't have fresh data, fetch it
@@ -26,6 +28,7 @@ export default class {
 			})
 			.catch(err => {
 				logger.error('Error fetching data from Redis', err, { cache_key: key });
+				metrics.histogram(`cache.${metricsKey}.error.time`, Date.now() - start)
 				metrics.count(`cache.${metricsKey}.error`, 1);
 				return this.fetchAndSet(key, ttl, fetcher);
 			});
@@ -36,21 +39,25 @@ export default class {
 			return this.currentRequests[key];
 		}
 		const metricsKey = key.split('.')[0];
-
+		let start = Date.now();
 		return this.currentRequests[key] = fetcher()
 			.then(res => {
 				if (res === undefined) {
 					return;
 				}
+				metrics.histogram(`cache.${metricsKey}.fresh.time`, Date.now() - start)
 				metrics.count(`cache.${metricsKey}.fresh`, 1);
 				const data = JSON.stringify(res);
+				start = Date.now();
 				return this.redis
 					.set(key, ttl, data)
 					.then(() => {
+						metrics.histogram(`cache.${metricsKey}.write.time`, Date.now() - start)
 						metrics.count(`cache.${metricsKey}.write`, 1);
 					})
 					.catch(err => {
 						logger.error('Error writing data to Redis', err, { cache_key: key });
+						metrics.histogram(`cache.${metricsKey}.error.time`, Date.now() - start)
 						metrics.count(`cache.${metricsKey}.error`, 1);
 					})
 					.then(() => {
